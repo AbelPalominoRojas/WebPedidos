ee5fba89697fdaa00b066ce7d820f11bff76259e
Abel Palomino <abel_17_87@hotmail.com>
2019-05-04 01:18:52 -0500

    Componente Ejemplo

--- apr.WebMVC/src/components/ExampleComponent.vue
+++ apr.WebMVC/src/components/ExampleComponent.vue
@@ -11,35 +11,6 @@
                 </div>
             </div>
 
-            <div class="col-md-8 pt-3">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" @click="showModal()">
-                    Launch demo modal
-                </button>
-            </div>
-
-        </div>
-
-    <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="close" @click="hideModal()">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" @click="hideModal()">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
         </div>
 
     </div>
@@ -48,23 +19,6 @@
 <script>
     export default {
         methods:{
-            showModal(){
-                console.log('click');
-                $('#exampleModal').modal('show');
-
-                axios.get('https://vuetable.ratiw.net/api/users')
-                .then(function (response) {
-                    // handle success
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-            },
-            hideModal(){
-                $('#exampleModal').modal('hide');
-            }
         },
         mounted() {
             console.log('Component mounted.')
